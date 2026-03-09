38d2ecac81eaad22e2887717641458e1e971e1ea
milene <mimi0003@stud.kea.dk>
2026-03-09 15:42:52 +0100

    work+about link

--- work.js
+++ work.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
           the final design into code. The process included iterative testing and 
           refinement to ensure a cohesive visual identity and improved usability 
           across devices.
-        </p>
+        </p><br>
     
        <br> <a href="https://tango-urquiza-projekt.netlify.app/" target="_blank">Visit Website</a>
       `,
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <p>Blooming Beauty is a fictional skincare brand created to explore brand identity, user experience, and interactive design. The goal was to develop a visually cohesive and engaging universe that appealed to a younger audience while maintaining trust and inclusivity.</p>
     
         <h3>The Solution</h3>
-       <p> My role focused on concept and experience design. I developed prototypes, generated AI visuals aligned with the brand’s tone of voice, designed and coded an interactive “Discover Your Skin Type” quiz to guide users toward relevant products. To strengthen engagement, I created a timed pop-up inviting users to “Join our Blooming Community,” reinforcing an inclusive and welcoming brand feel.</p>
+       <p> My role focused on concept and experience design. I developed prototypes, generated AI visuals aligned with the brand’s tone of voice, designed and coded an interactive “Discover Your Skin Type” quiz to guide users toward relevant products. To strengthen engagement, I created a timed pop-up inviting users to “Join our Blooming Community,” reinforcing an inclusive and welcoming brand feel.</p><br>
     
         <br><a href="https://jest0003.github.io/gruppe_1_t8/index.html" target="_blank">Visit Website</a>
       `,
@@ -45,19 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
        <p> The goal was to create a digital space that could present my poetry and photography in a cohesive and atmospheric way — without overdesigning or distracting from the art itself. The challenge was balancing simplicity with emotional depth.</p>
     
         <h3>The Solution</h3>
-        <p>I designed and coded a minimal, 90s-inspired interface that allows each poem and image to stand independently. I collaborated with photographers to ensure visual coherence between text and imagery. Each section includes a voice note (MP3) of me reading a poem, adding intimacy and dimensionality to the experience. As a dedicated writer of poetry, this project highlights my strength in storytelling, tone, and conceptual cohesion — skills that strongly inform my design work.</p>
-    
-        <br><a href="#" target="_blank">Visit Website</a>
-      `,
-    },
-    toki: {
-      title: "Tòki",
-      description: `
-        <h3>The Challenge</h3>
-       <p> Toki was a newly established B2B packaging company without existing cases or a defined visual identity. The challenge was to create a strong, credible brand presence and communicate their five-phase “Toki Flow” model.</p>
-    
-        <h3>The Solution</h3>
-       <p> I contributed by developing multiple interactive prototypes in Figma and refining the layout structure across key pages, including the Toki Flow model and case overview. Through iterative user testing and adjustments,  adjustments were made in regards to  strengthening navigation clarity , implementing a structured contact page.</p>
+        <p>I designed and coded a minimal, 90s-inspired interface that allows each poem and image to stand independently. I collaborated with photographers to ensure visual coherence between text and imagery. Each section includes a voice note (MP3) of me reading a poem, adding intimacy and dimensionality to the experience. As a dedicated writer of poetry, this project highlights my strength in storytelling, tone, and conceptual cohesion — skills that strongly inform my design work.</p><br>
     
        <br><a href="#" target="_blank">Visit Website</a>
       `,
